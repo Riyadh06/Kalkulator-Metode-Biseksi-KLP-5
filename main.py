8ef9db24802c029d92e88dc644f89eafd9ecef88
Muhammad Riyadh <riyadh24@mhs.usk.ac.id>
2025-10-26 22:14:07 +0700

    Create UI elements for user input and results
    Added UI widgets for input and results display.

--- main.py
+++ main.py
@@ -45,3 +45,50 @@ class BisectionCalculator(QWidget):
         self.move(qr.topLeft())
 
     def _create_widgets(self):
+        """Membuat semua widget yang dibutuhkan untuk UI."""
+
+        self.pers_label = QLabel("Masukkan persamaan:")
+        self.input_pers = QLineEdit()
+        self.input_pers.setPlaceholderText("Contoh: x**3 + 2*x**2 + 3*x - 4 ")
+
+        self.a_label = QLabel("Nilai tebakan a:")
+        self.input_a = QLineEdit()
+
+        self.b_label = QLabel("Nilai tebakan b:")
+        self.input_b = QLineEdit()
+
+        self.tolerance_label = QLabel("Nilai toleransi error (e):")
+        self.input_tolerance = QLineEdit()
+
+        self.iterations_label = QLabel("Jumlah maksimum iterasi (N):")
+        self.input_iterasi = QLineEdit()
+
+        # --- Buttons ---
+        self.tombol_hitung = QPushButton("Hitung")
+        self.tombol_hapus = QPushButton("Hapus")
+        self.tombol_hapus.setObjectName("tombol_hapus")
+
+        # --- Results Table ---
+        self.table = QTableWidget()
+        self.table.setColumnCount(9)
+        self.table.setHorizontalHeaderLabels(['Iterasi', 'a', 'b', 'f(a)', 'f(b)', 'x', 'f(x)', 'f(x) * f(a)', '|b - a|'])
+        # Mengatur agar lebar kolom menyesuaikan isi
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+    
+    def _setup_layout(self):
+        """Mengatur tata letak widget dalam form layout."""
+        layout = QFormLayout(self)
+        layout.setSpacing(10)
+        
+        layout.addRow(self.pers_label, self.input_pers)
+        layout.addRow(self.a_label, self.input_a)
+        layout.addRow(self.b_label, self.input_b)
+        layout.addRow(self.tolerance_label, self.input_tolerance)
+        layout.addRow(self.iterations_label, self.input_iterasi)
+        layout.addRow(self.tombol_hapus, self.tombol_hitung)
+        layout.addRow(self.table)
+        
+        self.setLayout(layout)
+
+    def _connect_signals(self):
+        """Menghubungkan sinyal (seperti klik tombol) ke slot (metode)."""
